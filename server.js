4b5e36790f4bb55dc9714e4f59537858e01f8b99
Juan Salvatore <juansalvatore@live.com.ar>
2018-10-12 01:15:38 -0300

    Add path to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+var path = require('path')
 
 const contacts = require('./routes/api/contacts')
 
@@ -42,16 +43,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'))
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  // })
-
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'), err => {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
